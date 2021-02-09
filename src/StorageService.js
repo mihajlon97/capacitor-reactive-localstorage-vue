a58d4bf1a190b8f185d893f41fa30c6a4d0fa70d
Mihajlo Nikodijevic <mihajlo.nikodijevic@3-s.at>
2021-02-09 01:45:51 +0100

    - capacitor storage api  - replace localStorage functions with async/await from capacitor

---
+++ src/StorageService.js
@@ -0,0 +1,38 @@
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
+
+export default {
+    async setObject(key, object) {
+        await Storage.set({
+            key,
+            value: JSON.stringify(object)
+        });
+    },
+
+    async getObject(key) {
+        const res = await Storage.get({ key });
+        return JSON.parse(res.value);
+    },
+
+    async setItem(key, value) {
+        return await Storage.set({ key, value });
+    },
+
+    async getItem(key) {
+        const { value } = await Storage.get({ key });
+        return value
+    },
+
+    async removeItem(key) {
+        return await Storage.remove({ key });
+    },
+
+    async keys() {
+        const { keys } = await Storage.keys();
+        return keys
+    },
+
+    async clear() {
+        return await Storage.clear();
+    },
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const keyBase = "vrs_";
-
+import StorageService from "./StorageService";
 const store = {
-    _initialize(defaults) {
-        let json = window.localStorage.getItem(keyBase);
+    async _initialize(defaults) {
+        let json = await StorageService.getItem(keyBase);
         if (json === null) {
-            window.localStorage.setItem(keyBase, JSON.stringify(defaults));
+            await StorageService.setItem(keyBase, JSON.stringify(defaults));
         } else {
             let storage = JSON.parse(json);
             this._objectDefaults(defaults, storage);
-            window.localStorage.setItem(keyBase, JSON.stringify(storage));
+            await StorageService.setItem(keyBase, JSON.stringify(storage));
         }
     },
     _objectDefaults(object, storage) {
@@ -24,61 +24,61 @@ const store = {
             return acc;
         }, []);
     },
-    getRaw() {
-        let json = window.localStorage.getItem(keyBase);
+    async getRaw() {
+        let json = await StorageService.getItem(keyBase);
         return JSON.parse(json);
     },
-    setRaw(object) {
+    async setRaw(object) {
         let json = JSON.stringify(object);
-        window.localStorage.setItem(keyBase, json);
+        await StorageService.setItem(keyBase, json);
     },
-    set(key, value) {
+    async set(key, value) {
         if (value === undefined) {
             return this.remove(key);
         }
         // Set value
-        window.localStorage.setItem(keyBase + key, this.serialize(value));
+        await StorageService.setItem(keyBase + key, this.serialize(value));
         // Store key
         let keys = this.getKeys();
         if (keys.indexOf(key) === -1) {
             keys.push(key);
         }
-        window.localStorage.setItem(keyBase, this.serialize(keys));
+        await StorageService.setItem(keyBase, this.serialize(keys));
 
         return value;
     },
-    get(key) {
-        let value = window.localStorage.getItem(keyBase + key);
+    async get(key) {
+        let value = await StorageService.getItem(keyBase + key);
         return value === null ? null : this.deserialize(value);
     },
-    remove(key) {
-        let value = this.get(key);
+    async remove(key) {
+        let value = await this.get(key);
         // Remove value
-        window.localStorage.removeItem(keyBase + key);
+        await StorageService.removeItem(keyBase + key);
         // Remove key
-        let keys = this.getKeys();
+        let keys = await this.getKeys();
         let index = keys.indexOf("test");
         if (index !== -1) keys.splice(index, 1);
-        window.localStorage.setItem(keyBase, this.serialize(keys));
+        await StorageService.setItem(keyBase, this.serialize(keys));
 
         return value;
     },
-    getAll() {
+    async getAll() {
         let items = [];
-        let keys = this.getKeys();
+        let keys = await this.getKeys();
         for (let i = 0; i < keys.length; i++) {
             items[keys[i]] = this.get(keys[i]);
         }
         return items;
     },
-    getKeys() {
-        return this.deserialize(window.localStorage.getItem(keyBase));
+    async getKeys() {
+        return await this.deserialize(StorageService.getItem(keyBase));
     },
-    clear() {
-        this.set(keyBase, "[]");
+    async clear() {
+        await this.set(keyBase, "[]");
         let keys = this.getKeys();
         for (let i = 0; i < keys.length; i++) {
-            this.remove(keyBase + keys[i]);
+            await this.remove(keyBase + keys[i]);
         }
     },
     serialize: function (object) {
@@ -95,9 +95,9 @@ const store = {
 
 const ReactiveStorage = {
     store,
-    install(Vue, options) {
-        store._initialize(options);
-        let values = store.getRaw();
+    async install(Vue, options) {
+        await store._initialize(options);
+        let values = await store.getRaw();
 
         Vue.mixin({
             data() {
@@ -109,8 +109,8 @@ const ReactiveStorage = {
             },
             watch: {
                 storage: {
-                    handler() {
-                        store.setRaw(this.storage);
+                    async handler() {
+                        await store.setRaw(this.storage);
                     },
                     deep: true
                 }
